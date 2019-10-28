275b8bd9941a092cce24c68cf60e29e8ccfb175f
Toyin Adesina <toyinadesina60@gmail.com>
2019-10-28 19:23:53 +0100

    Commit after task 2 correction

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this._albumUrl).pipe(map((response: any) => response.json()))
+    return this._http.get(this._albumUrl).map((response) => response.json())
   }
 }
